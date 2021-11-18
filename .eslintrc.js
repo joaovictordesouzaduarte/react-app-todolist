664f40ba69a28e991166eb1a08c30f2550871c3f
jvduarte <victorduarte.ufrj@gmail.com>
2021-11-18 20:23:52 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'react/function-component-definition': 'off',
+    'react/state-in-constructor': 'off',
+    'react/no-unused-state': 'off',
+  },
+};

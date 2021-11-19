e9b0531e4e6b942bb22aea5f0aa989e6b3d200f5
jvduarte <victorduarte.ufrj@gmail.com>
2021-11-18 21:15:57 -0300

    edit funcionality

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,17 +5,42 @@ import './Main.css';
 export default class Main extends Component {
   state = {
     novaTarefa: '',
-    tarefas: [
-      'Fazer café',
-      'Estudar',
-      'Beber água',
-    ],
+    tarefas: [],
   };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { tarefas } = this.state;
+    let { novaTarefa } = this.state;
+    novaTarefa = novaTarefa.trim();
+
+    if (tarefas.indexOf(novaTarefa) !== -1) return;
+
+    const novasTarefas = [...tarefas];
+
+    this.setState({
+      tarefas: [...novasTarefas, novaTarefa],
+    });
+  }
 
   handleChange = (e) => {
     this.setState({
       novaTarefa: e.target.value,
     });
+  }
+
+  handleDelete = (e, index) => {
+    const { tarefas } = this.state;
+    const novasTarefas = [...tarefas];
+    novasTarefas.splice(index, 1);
+
+    this.setState({
+      tarefas: [...novasTarefas],
+    });
+  }
+
+  handleEdit = (e, index) => {
+    console.log('Edit', index, e);
   }
 
   render() {
@@ -24,20 +49,20 @@ export default class Main extends Component {
       <div className="main">
         <h1>Lista de Tarefas</h1>
 
-        <form action="#" className="form">
+        <form onSubmit={this.handleSubmit} action="#" className="form">
           <input onChange={this.handleChange} type="text" value={novaTarefa} />
           <button type="submit">
             <FaPlus />
           </button>
         </form>
         <ul className="tarefas">
-          {tarefas.map((tarefa) => (
+          {tarefas.map((tarefa, index) => (
             <li key={tarefa}>
               {tarefa}
-              <div>
-                <FaEdit className="edit" />
-                <FaWindowClose className="delete" />
-              </div>
+              <span>
+                <FaEdit onClick={(e) => this.handleEdit(e, index)} className="edit" />
+                <FaWindowClose onClick={(e) => this.handleDelete(e, index)} className="delete" />
+              </span>
             </li>
           ))}
         </ul>
